70265b88bb7dc7241bc58c0f5eb5ae592ce8ddee
Vincent Ekpanyaskun <vincent.ekp@gmail.com>
2024-10-19 06:18:37 +1100

    serial print touch readings

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int pins[] = {4, 0, 2, 15, 13, 12, 14, 27, 33, 32};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Touch sensor readings");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int i = 0; i < 10; i++) {
+    uint8_t touchReading = touchRead(pins[i]);
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.print(touchReading);
+    Serial.print(", ");
+  }
+  Serial.println("\n");
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
