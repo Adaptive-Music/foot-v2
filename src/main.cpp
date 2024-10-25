41f9b837296c65b130524339ca3e467b7841e303
Vincent Ekpanyaskun <vincent.ekp@gmail.com>
2024-10-25 22:29:21 +1100

    Implement button for change mode toggle between drum and single note

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int pins[] = {T6, T4, T5, T7, T8, T3, T2, T9};
 
 // Touch reading thresholds below which touchRead value means sensor is touched
-int thresholds[] = {27, 29, 33, 28, 27, 24, 24, 30};
+int thresholds[] = {27, 31, 33, 28, 27, 24, 24, 30};
 
 // Button for mode change
 int buttonPin = T0;
@@ -15,11 +15,44 @@ int buttonThreshold = 52;
 bool oldState[8] = {false};
 bool newState[8] = {false};
 
-// MIDI notes to send
-int defaultDrum[8] = {36, 38, 37, 43, 45, 42, 46, 49};
-int majorScale[8] = {60, 62, 64, 65, 67, 69, 71, 72};
-bool drumMode = false;
+// Define key - initially set to C4 (Middle C)
+int key = 60;
 
+// Define notes to be played by each button - numbers represent how many semitones above tonic (except for drum).
+int scales[][8] = {
+  {36, 38, 37, 43, 45, 42, 46, 49}, // Drum
+  {0, 2, 4, 5, 7, 9, 11, 12},   // Major
+  {0, 2, 3, 5, 7, 8, 10, 12},   // Minor
+  {0, 3, 5, 7, 10, 12, 15, 17},  // Pentatonic
+};
+
+
+// // MIDI notes to send
+// int defaultDrum[8] = {36, 38, 37, 43, 45, 42, 46, 49};
+// int majorScale[8] = {60, 62, 64, 65, 67, 69, 71, 72};
+
+const int DRUM_MODE = 0;
+const int SINGLE_NOTE = 1;
+
+const int NUM_MODES = 2;
+
+int currentMode = DRUM_MODE;
+
+
+void silence() {
+  // Stop all currently playing notes
+  for (int i = 0; i < 128; i++) {
+    BLEMidiServer.noteOff(0, i, 127);
+    delay(5);
+  }
+}
+
+void changeMode() {
+  silence();
+  currentMode = (currentMode + 1) % NUM_MODES;
+  Serial.printf("New mode: %d\n", currentMode);
+  delay(1000);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -27,7 +60,10 @@ void setup() {
 }
 
 void loop() {
-  if 
+  if (touchRead(buttonPin) < buttonThreshold) {
+    changeMode();
+    return;
+  }
 
    // Read and store the current state of the pushbutton values
   for (int i = 0; i < 8; i++) newState[i] = touchRead(pins[i]) < thresholds[i];
@@ -37,10 +73,12 @@ void loop() {
     if (oldState[i] == newState[i]) continue;
     // Update oldState with changed value
     oldState[i] = newState[i];
-    if (!BLEMidiServer.isConnected()) break;
+    if (!BLEMidiServer.isConnected()) continue;
     // Play/end note if pressed/released
-    if (newState[i]) BLEMidiServer.noteOn(0, majorScale[i], 127);
-    else BLEMidiServer.noteOff(0, majorScale[i], 127);
+    int note = scales[currentMode][i] + (currentMode == DRUM_MODE? 0 : key); 
+    Serial.println(note);
+    if (newState[i]) BLEMidiServer.noteOn(0, note, 127);
+    else BLEMidiServer.noteOff(0, note, 127);
   }
   // Delay for debouncing
   delay(10);
